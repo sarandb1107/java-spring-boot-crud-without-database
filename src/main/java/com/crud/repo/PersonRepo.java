ca59060c7b1c001433475a6cbb1ce90c2a30b15f
sarandb1107 <sarandb1107@gmail.com>
2023-08-30 17:54:26 +0530

    code changes again

--- src/main/java/com/crud/repo/PersonRepo.java
+++ src/main/java/com/crud/repo/PersonRepo.java
@@ -13,7 +13,7 @@ public class PersonRepo {
 
 	private List<Person> list = new ArrayList<Person>();
 
-	public List<Person> getAll() {
+	public void getAll() {
 		list.add(new Person(1, "Trinh Minh Cuong", "Developer", true, "1975-11-27"));
 		list.add(new Person(2, "Mary Jane", "Banker", false, "1980-05-24"));
 		list.add(new Person(3, "Tom Sawyer", "Taxi Driver", true, "1990-08-09"));
